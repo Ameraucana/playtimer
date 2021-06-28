35055ac0f4538b2da3c810b0186984ae66ab8f1e
Wyandotte <daltdalt1218@comcast.net>
2021-06-28 13:51:57 -0400

    UnsavedChangeModel is now notified before and in the periodic timer

--- lib/classes/Timekeeper.dart
+++ lib/classes/Timekeeper.dart
@@ -14,6 +14,7 @@ class Timekeeper {
     model.madeChange();
     repeatingTimer = Timer.periodic(Duration(seconds: 1), (_) {
       setState(() => seconds++);
+      model.madeChange();
     });
   }
   void stop() {
