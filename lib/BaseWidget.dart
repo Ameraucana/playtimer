efaccb4b805f7b6d0f3d3c21f8256c8917a384d2
Wyandotte <daltdalt1218@comcast.net>
2021-09-11 02:11:48 -0500

    syncs data online; data stored in folder locally

--- lib/BaseWidget.dart
+++ lib/BaseWidget.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'package:playtimer/BaseWidget/LastChange.dart';
@@ -17,7 +18,6 @@ import 'classes/TimedItem.dart';
 import 'classes/UnsavedChangeModel.dart';
 import 'classes/Timekeeper.dart';
 
-
 class BaseWidget extends StatefulWidget {
   BaseWidget({@required this.timedItems});
   final List<TimedItem> timedItems;
@@ -31,12 +31,15 @@ class BaseWidgetState extends State<BaseWidget> {
     timekeeper = Timekeeper(setState)
       ..activeItem = timedItems[0]
       ..model = unsavedChangeModel
-      ..seconds = timedItems.singleWhere((item) => item.name == timedItems[0].name).seconds;
-   }
+      ..seconds = timedItems
+          .singleWhere((item) => item.name == timedItems[0].name)
+          .seconds;
+  }
   Timekeeper timekeeper;
   List<TimedItem> timedItems;
   UnsavedChangeModel unsavedChangeModel;
   bool startButtonIsActive = true;
+  final String urlEndpoint = r"https://json.psty.io/api_v1/stores/playtimer";
 
   void addNew(TimedItem item) {
     setState(() {
@@ -48,10 +51,11 @@ class BaseWidgetState extends State<BaseWidget> {
     int currentIndex = timedItems.indexOf(item);
     print("currentIndex: $currentIndex");
     if (currentIndex > 0) {
-      setState(() => timekeeper.select(timedItems[currentIndex-1]));
+      setState(() => timekeeper.select(timedItems[currentIndex - 1]));
     } else {
       if (timedItems.length > 1) {
-        setState(() => setState(() => timekeeper.select(timedItems[currentIndex+1])));
+        setState(() =>
+            setState(() => timekeeper.select(timedItems[currentIndex + 1])));
       } else {
         return;
       }
@@ -60,18 +64,28 @@ class BaseWidgetState extends State<BaseWidget> {
       timedItems.remove(item);
     });
   }
+
   void rewrite() async {
     Directory documentsDir = await getApplicationDocumentsDirectory();
     print(documentsDir.path);
-    File file = File(path.join(documentsDir.path, "timedItems.json"));
+    File file = File(path.join(documentsDir.path, "playtimer_data", "timedItems.json"));
+    await file.create(recursive: true);
     String output = TimedItem.encode(timedItems);
+    try {
+      http.Response response = await http.put(Uri.parse(urlEndpoint),
+          headers: {
+            "Content-Type": "application/json",
+            "Api-Key": await rootBundle.loadString("assets/key")
+          },
+          body: output);
+      print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
     await file.writeAsString(output);
   }
 
-  TextStyle style = TextStyle(
-    fontFamily: "DSEG",
-    fontSize: 30
-  );
+  TextStyle style = TextStyle(fontFamily: "DSEG", fontSize: 30);
   FocusNode keyboardListenerFocusNode = FocusNode();
   @override
   Widget build(context) {
@@ -83,12 +97,9 @@ class BaseWidgetState extends State<BaseWidget> {
         onKey: (RawKeyEvent event) {
           if (event.logicalKey == LogicalKeyboardKey.escape) {
             keyboardListenerFocusNode.requestFocus();
-          }
-          else if (
-            event.logicalKey == LogicalKeyboardKey.space
-            && event.runtimeType.toString() == "RawKeyDownEvent"
-            && keyboardListenerFocusNode.hasPrimaryFocus
-          ) {
+          } else if (event.logicalKey == LogicalKeyboardKey.space &&
+              event.runtimeType.toString() == "RawKeyDownEvent" &&
+              keyboardListenerFocusNode.hasPrimaryFocus) {
             if (!timekeeper.isRunning) {
               setState(() {
                 timekeeper.start();
@@ -103,51 +114,45 @@ class BaseWidgetState extends State<BaseWidget> {
           }
         },
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GroupingBox(
-              children: [
-                TimeDisplay(seconds: timekeeper.seconds, mergeSeconds: (definedSeconds) {
-                  setState(() => timekeeper.merge(definedSeconds));
-                }),
-
-                SizedBox(width: 0, height: 10),
-                StartStopButton(
-                  startButtonFunc: () {
-                    setState(() {
-                      timekeeper.start();
-                      startButtonIsActive = false;
-                    });
-                  },
-                  stopButtonFunc: () {
-                    setState(() {
-                      timekeeper.stop();
-                      startButtonIsActive = true;
-                    });
-                  },
-                  startButtonIsActive: startButtonIsActive,
-                )
-              ]
-            ),
+            GroupingBox(children: [
+              TimeDisplay(
+                  seconds: timekeeper.seconds,
+                  mergeSeconds: (definedSeconds) {
+                    setState(() => timekeeper.merge(definedSeconds));
+                  }),
+              SizedBox(width: 0, height: 10),
+              StartStopButton(
+                startButtonFunc: () {
+                  setState(() {
+                    timekeeper.start();
+                    startButtonIsActive = false;
+                  });
+                },
+                stopButtonFunc: () {
+                  setState(() {
+                    timekeeper.stop();
+                    startButtonIsActive = true;
+                  });
+                },
+                startButtonIsActive: startButtonIsActive,
+              )
+            ]),
             SizedBox(height: 10, width: 0),
-            GroupingBox(
-              children: [
-                ItemCreator(creationCallback: addNew)
-              ]
-            ),
+            GroupingBox(children: [ItemCreator(creationCallback: addNew)]),
             SizedBox(height: 10, width: 0),
             GroupingBox(
               children: [
                 Selector(
-                  items: timedItems,
-                  onSelect: (newItem) => setState(() {
-                    timekeeper.select(newItem);
-                    setState(() => startButtonIsActive = true);
-                    print("activeItem is now ${timekeeper.activeItem}");
-                  }),
-                  onRemove: remove
-                ),
+                    items: timedItems,
+                    onSelect: (newItem) => setState(() {
+                          timekeeper.select(newItem);
+                          setState(() => startButtonIsActive = true);
+                          print("activeItem is now ${timekeeper.activeItem}");
+                        }),
+                    onRemove: remove),
                 LastChange(lastChangeDate: timekeeper.activeItem.lastChangeDate)
               ],
             ),
