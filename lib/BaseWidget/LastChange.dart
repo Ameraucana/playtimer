57cf5c2e8d43000e501c5a157af1b791b6eddba1
Wyandotte <daltdalt1218@comcast.net>
2021-12-14 15:33:28 -0600

    refined LastChange.dart

--- lib/BaseWidget/LastChange.dart
+++ lib/BaseWidget/LastChange.dart
@@ -4,8 +4,25 @@ class LastChange extends StatelessWidget {
   LastChange({@required this.lastChangeDate});
   final DateTime lastChangeDate;
 
-  String formatter() {
+  String date() {
     return "${lastChangeDate.month}/${lastChangeDate.day}/${lastChangeDate.year}";
+  }
+  String time() {
+    int to12Time() {
+      int convertedTime = lastChangeDate.hour % 12;
+      if (convertedTime == 0) {
+        convertedTime = 12;
+      }
+      return convertedTime;
+    }
+    String amOrPm() {
+      if (lastChangeDate.hour < 12) {
+        return "AM";
+      } else {
+        return "PM";
+      }
+    }
+    return "${to12Time()}:${lastChangeDate.minute.toString().padLeft(2, "0")}${amOrPm()}";
   }
 
   @override
@@ -14,8 +31,10 @@ class LastChange extends StatelessWidget {
       text: TextSpan(
         style: DefaultTextStyle.of(context).style,
         children: <TextSpan>[
-          TextSpan(text: "Last changed on "),
-          TextSpan(text: formatter())
+          TextSpan(text: "Last changed on   "),
+          TextSpan(text: date(), style: TextStyle(backgroundColor: Color(0xFF00008F))),
+          TextSpan(text: " at "),
+          TextSpan(text: time(), style: TextStyle(backgroundColor: Color(0xFF00008F))),
         ]
       )
     );
